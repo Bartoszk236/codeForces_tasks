7c56dbb5e53225664294d0a9e7c48baf67d35ec1
bartoszkocylo <bartoszk236@gmail.com>
2024-12-18 12:56:30 +0100

    10 problems repair

--- src/problem228a/Main.java
+++ src/problem228a/Main.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
+    public static final int NUMBER_OF_HORSE_SHOES = 4;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < NUMBER_OF_HORSE_SHOES; i++) {
             set.add(scanner.nextInt());
         }
-        System.out.println(4 - set.size());
+        System.out.println(NUMBER_OF_HORSE_SHOES - set.size());
     }
 }
