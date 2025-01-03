b2c5f64d753d9b1b0415b8707760bb5c66c20b94
bartoszkocylo <bartoszk236@gmail.com>
2025-01-03 17:07:20 +0100

    problem 723a solution

---
+++ src/problem723a/Main.java
@@ -0,0 +1,24 @@
+package src.problem723a;
+//https://codeforces.com/problemset/problem/723/A
+//16:03 - time of solution
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+        List<Integer> list = new ArrayList<>();
+        list.add(x);
+        list.add(y);
+        list.add(z);
+        list.sort(Integer::compareTo);
+        int center = list.get(1);
+        int sum = Math.abs(x - center) + Math.abs(y - center) + Math.abs(z - center);
+        System.out.println(sum);
+    }
+}
