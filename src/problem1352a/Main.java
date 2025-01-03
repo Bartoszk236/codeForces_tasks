f75eb1b7a8368d326a0ff5ca3de2e27babe738fb
bartoszkocylo <bartoszk236@gmail.com>
2025-01-03 17:07:14 +0100

    problem 1352a solution

---
+++ src/problem1352a/Main.java
@@ -0,0 +1,38 @@
+package src.problem1352a;
+//https://codeforces.com/problemset/problem/1352/A
+//53:30 - time of solution
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt();
+            List<Integer> results = solve(n);
+            System.out.println(results.size());
+            for (int j = 0; j < results.size(); j++) {
+                System.out.print(results.get(j) + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static List<Integer> solve(int n) {
+        char[] chars = String.valueOf(n).toCharArray();
+        List<Integer> res = new ArrayList<>();
+        int pow = 0;
+        for (int i = chars.length - 1 ; i >= 0; i--) {
+            String s = String.valueOf(chars[i]);
+            BigInteger integer = new BigInteger(s);
+            BigInteger result = integer.multiply(BigInteger.valueOf(10).pow(pow));
+            if (!result.equals(BigInteger.ZERO)) res.add(result.intValue());
+            pow++;
+        }
+        return res;
+    }
+}
