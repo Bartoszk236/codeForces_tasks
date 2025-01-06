d0b2ccef695ba7e1545789ecd837d2ec5fffee78
bartoszkocylo <bartoszk236@gmail.com>
2025-01-06 15:21:41 +0100

    problem 630a solution

---
+++ src/problem630a/Main.java
@@ -0,0 +1,14 @@
+package src.problem630a;
+//https://codeforces.com/problemset/problem/630/A
+//22:30 - time of solution
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger n = new BigInteger(scanner.nextLine());
+        System.out.println(25);
+    }
+}
