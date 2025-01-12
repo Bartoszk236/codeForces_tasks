7e988733dd9548c2a75ae82e26e06a6410eea631
bartoszkocylo <bartoszk236@gmail.com>
2025-01-12 17:19:34 +0100

    solve the problem 1760a, 758a

---
+++ src/problem1760a/Main.java
@@ -0,0 +1,29 @@
+package src.problem1760a;
+//https://codeforces.com/problemset/problem/1760/A
+//3:10 - time of solution
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static final int COUNT_OF_NUMBERS = 3;
+    public static final int INDEX_OF_MEDIAN_NUMBER = 1;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < COUNT_OF_NUMBERS; j++) {
+                list.add(scanner.nextInt());
+            }
+            System.out.println(solve(list));
+        }
+    }
+
+    public static int solve(List<Integer> list) {
+        list.sort(Integer::compareTo);
+        return list.get(INDEX_OF_MEDIAN_NUMBER);
+    }
+}
