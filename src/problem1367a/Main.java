02469b89ed44cbdd1ce129cde815e858d14f2d23
bartoszkocylo <bartoszk236@gmail.com>
2025-01-13 19:50:49 +0100

    solve the problem 1367a

---
+++ src/problem1367a/Main.java
@@ -0,0 +1,29 @@
+package src.problem1367a;
+//https://codeforces.com/problemset/problem/1367/A
+//29:28 - time of solution
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < t; i++) {
+            String line = scanner.nextLine();
+            System.out.println(solve(line));
+        }
+    }
+
+    public static String solve(String line) {
+        String end = line.substring(line.length() - 2);
+        String inside = line.substring(0, line.length() - 2);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < inside.length(); i++) {
+            if (i % 2 == 0) sb.append(inside.charAt(i));
+        }
+        sb.append(end);
+        return sb.toString();
+    }
+}
